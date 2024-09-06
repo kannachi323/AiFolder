8e1a512b77080a7fc2428c363ce96329b8bf7085
kannachi323 <mattdev727@gmail.com>
2024-09-06 14:19:24 -0700

    new folder structure

--- server/APIs/ai/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-from . import gpt
-
-
-ai_bp = Blueprint('ai', __name__)
-
-@ai_bp.route('/callChatGPT', methods=['GET', 'POST'])
-def callChatGPT():  
-    return gpt.callChatGPT()
